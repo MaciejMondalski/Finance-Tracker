f8d6f8d7f662dd77d31f7f5b7e62718a3f6bc71d
MaciejMondalski <maciej.mondalski@gmail.com>
2022-02-27 17:38:42 +0100

    added gatekeeping from /home for not logged users

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useAuthContext } from './hooks/useAuthContext';
+// pages & components
 import Navbar from './components/Navbar';
-import Home from './pages/Home';
+import Home from './pages/home/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 
 function App() {
+  const { authIsReady, user } = useAuthContext();
+
   return (
     <div className='App'>
-      <BrowserRouter>
-        <Navbar />
+      {authIsReady && (
+        <BrowserRouter>
+          <Navbar />
 
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='login' element={<Login />} />
-          <Route path='signup' element={<Signup />} />
-        </Routes>
-      </BrowserRouter>
+          <Routes>
+            <Route
+              path='/'
+              element={!user ? <Navigate to='/login' /> : <Home />}
+            />
+            <Route
+              path='login'
+              element={user ? <Navigate to='/' /> : <Login />}
+            />
+            <Route
+              path='signup'
+              element={user ? <Navigate to='/' /> : <Signup />}
+            />
+          </Routes>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
