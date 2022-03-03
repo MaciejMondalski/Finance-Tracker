5f0244a04b8cb1fca5b3784069182d2ed079ccb8
MaciejMondalski <maciej.mondalski@gmail.com>
2022-03-03 22:18:22 +0100

    .

---
+++ src/hooks/useCollection.js
@@ -0,0 +1,11 @@
+import { useEffect } from 'react';
+import { projectFirestore } from '../firebase/config';
+
+export const useCollection = (collection) => {
+  const [documents, setDocuments] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    let ref = projectFirestore.collection(collection);
+  }, [collection]);
+};
