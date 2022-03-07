b9032a08c6f91b960ad5a29a599269472325219c
MaciejMondalski <maciej.mondalski@gmail.com>
2022-03-07 23:15:49 +0100

    added styles to the transaction list

--- src/pages/home/TransactionList.js
+++ src/pages/home/TransactionList.js
@@ -3,19 +3,62 @@ import styled from 'styled-components';
 export const TransactionList = ({ transactions }) => {
   return (
     <StyledTransactionList>
-      {transactions.map((transaction) => {
-        <li key={transaction.id}>
-          <h1>dsa</h1>
-          <p className='name'>{transaction.name}</p>
-          <p className='amount'>{transaction.amount}</p>
-        </li>;
-      })}
+      <ul className='transactions'>
+        {transactions.map((transaction) => (
+          <li key={transaction.id}>
+            <p className='name'>{transaction.name}</p>
+            <p className='amount'>${transaction.amount}</p>
+          </li>
+        ))}
+      </ul>
     </StyledTransactionList>
   );
 };
 
 const StyledTransactionList = styled.div`
+  width: 90%;
+
   li {
-    color: green;
+    padding: 20px;
+    margin: 30px auto;
+    box-shadow: 4px 4px 5px rgba(50, 50, 50, 0.1);
+    display: flex;
+    align-items: center;
+    position: relative;
+    overflow: hidden;
+    border-left: 4px solid #2c6eac;
+    transition-duration: 0.2s;
+
+    &:hover {
+      box-shadow: 5px 5px 6px 4px rgba(50, 50, 50, 0.08);
+      transform: scale(1.01);
+    }
+  }
+
+  .name {
+    color: #777;
+    font-size: 1.2em;
+  }
+
+  .amount {
+    margin-left: auto;
+    margin-right: 20px;
+    color: #777;
+    font-weight: bold;
+    font-size: 1.4em;
+  }
+
+  button {
+    position: absolute;
+    top: 0;
+    right: 0;
+    background: #ddd;
+    color: #777;
+    border: none;
+    padding: 12px 8px;
+    text-align: center;
+    line-height: 0;
+    font-size: 0.9em;
+    cursor: pointer;
   }
 `;
